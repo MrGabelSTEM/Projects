b685b1b570cc6e4b698482cc85c5a840cea1abcd
jade-hoppy <117459374+jade-hoppy@users.noreply.github.com>
2022-11-09 15:13:17 -0500

    first upload

---
+++ Create with Code Unit 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 20.5f;
+
+   public GameObject projectilePrefab;
+ 
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if(transform.position.x < -xRange)
+       {
+               transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+       }
+        
+       if(transform.position.x > xRange)
+       {
+               transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
+       }
+
+       //create another if-statement
+       if(Input.GetKeyDown(KeyCode.Space))
+       {
+        //launch a projectile from the player
+        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        //prefab to spawn at player position
+        //prefab rotation is stored in transform
+       }
+
+
+    }
+}
